e67fc3c9bacbe5609209892d294417dfb32cb7b3
chormoon <c1010472@newcastle.ac.uk>
2021-11-29 16:16:30 +0000

    remove na rows

---
+++ src/02-A.R
@@ -0,0 +1,22 @@
+#remove na rows
+
+archetype_survey_responses = na.omit(archetype_survey_responses2)
+
+enrolments = na.omit(enrolments2)
+
+leaving_survey_responses = na.omit(leaving_survey_responses2)
+
+question_response = na.omit(question_response2)
+
+step_activity = na.omit(step_activity2)
+
+team_members = na.omit(team_members2)
+
+video_stats = na.omit(video_stats2)
+
+weekly_sentiment_survey_responses = na.omit(weekly_sentiment_survey_responses2)
+
+
+
+
+#
